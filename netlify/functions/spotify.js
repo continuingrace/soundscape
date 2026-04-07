d471096499ed4d521753469a4d807f3ef1eeb375
continuingrace <continuingrace@gmail.com>
2026-04-07 09:29:47 +0900

    Update spotify.js

--- netlify/functions/spotify.js
+++ netlify/functions/spotify.js
@@ -24,7 +24,7 @@ exports.handler = async (event) => {
   }
 
   if (action === 'addTracks') {
-    const res = await fetch(`https://api.spotify.com/v1/playlists/${params.playlistId}/tracks`, {
+    const res = await fetch(`https://api.spotify.com/v1/playlists/${params.playlistId}/items`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
       body: JSON.stringify({ uris: params.uris }),
